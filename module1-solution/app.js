0c6d8d5c57763bb3ce7866c3d8e039a9bb8612fb
Conficlown <wwstargazer@hotmail.com>
2016-09-15 23:33:11 +0200

    First assignment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,26 +1,42 @@
 (function () {
 'use strict';
 
-angular.module('DIApp', [])
-.controller('DIController', DIController);
+angular.module('Module1', [])
+.controller('LunchController', LunchController);
 
-function DIController ($scope,
-                       $filter,
-                       $injector) {
-  $scope.name = "Yaakov";
+LunchController.$inject = ['$scope'];
+function LunchController ($scope) {
+  $scope.lunch = "";
+  $scope.comment = "";
 
-  $scope.upper = function () {
-    var upCase = $filter('uppercase');
-    $scope.name = upCase($scope.name);
+  $scope.Check = function () {
+    var temp = giveComment($scope.lunch);
+    $scope.comment = temp;
   };
 
-  console.log($injector.annotate(DIController));
-}
+  function giveComment(string) {
+    var splitArray = string.split(',');
+    var numDish = 0;
+    for (var i = 0; i < splitArray.length ; i++) {
+        if (!(/^\s*$/).test(splitArray[i]))
+        {
+          numDish += 1;
+        }
+      }
 
-function AnnonateMe(name, job, blah) {
-  return "Blah!";
-}
-
-console.log(DIController.toString());
+    if (numDish == 0)
+    {
+      return "";
+    }
+    else if (numDish <=3)
+    {
+      return "Enjoy!";
+    }
+    else
+    {
+      return "Too much!";
+    }
+  };
+};
 
 })();
