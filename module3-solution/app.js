0162ba132b4538c394fa0451755d1dd5cf9dde57
conficlown <wwstargazer@hotmail.com>
2018-11-26 23:55:13 +0100

    promise part done

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -5,12 +5,23 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-.directive('foundItems', foundItems);
+//.directive('foundItems', foundItems);
 
 NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+    var found = this;
 
+    found.narrowDown = function ( filterName ) {
+        var promise = MenuSearchService.getMatchedMenuItems(filterName);
 
-function ListItem() {
+        for ( var i in promise ){
+            console.log(promise[i]);
+        }
+    }
+
+}
+/*
+function foundItems() {
   var ddo = {
     templateUrl: 'foundItems.html',
     scope: {
@@ -20,22 +31,32 @@ function ListItem() {
 
   return ddo;
 }
-
+*/
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   var service = this;
+  var foundItems = [];
 
   service.getMatchedMenuItems = function (searchTerm) {
     var response = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json")
-    }).then(function (result) {
+  }).then(function success(result) {
         // process result and only keep items that match
-        var foundItems...
-
+        // console.log(result.data.menu_items);
+        for ( var i in result.data.menu_items) {
+            if ( result.data.menu_items[i].description.toLowerCase().indexOf( searchTerm.trim().toLowerCase() ) !== -1 ) {
+                foundItems.push(result.data.menu_items[i]);
+            }
+        }
+        // console.log(foundItems);
         // return processed items
         return foundItems;
-    });;
+    },
+    function error(result){
+        console.log("Something wrong");
+    }
+    );
 
     return response;
   };
